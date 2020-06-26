cab9de49fcd9a1022c7092b052fd878de8d7697c
idmacleod <idmacleod@gmail.com>
2020-06-26 19:00:48 +0100

    Implemented SheetMusic class

--- src/main/java/accessories/SheetMusic.java
+++ src/main/java/accessories/SheetMusic.java
@@ -1,4 +1,20 @@
 package accessories;
 
-public class SheetMusic {
+public class SheetMusic extends Accessory {
+    private String genre;
+    private String difficulty;
+
+    public SheetMusic(String description, int buyPrice, int sellPrice, String genre, String difficulty) {
+        super(description, buyPrice, sellPrice);
+        this.genre = genre;
+        this.difficulty = difficulty;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
 }
