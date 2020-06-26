7ea4ad408f650bb3cc031e8a7bc691da6c62c4d9
idmacleod <idmacleod@gmail.com>
2020-06-26 17:40:38 +0100

    Added Piano tests

--- src/test/java/instruments/PianoTest.java
+++ src/test/java/instruments/PianoTest.java
@@ -1,4 +1,56 @@
 package instruments;
 
+import org.junit.Before;
+import org.junit.Test;
+import types.PianoType;
+
+import static org.junit.Assert.assertEquals;
+
 public class PianoTest {
+    private Piano piano;
+
+    @Before
+    public void before() {
+        piano = new Piano("Yamaha", "CF3", 50000, 75000, PianoType.GRAND);
+    }
+
+    @Test
+    public void hasManufacturer() {
+        assertEquals("Yamaha", piano.getManufacturer());
+    }
+
+    @Test
+    public void hasModel() {
+        assertEquals("CF3", piano.getModel());
+    }
+
+    @Test
+    public void hasBuyPrice() {
+        assertEquals(50000, piano.getBuyPrice());
+    }
+
+    @Test
+    public void hasSellPrice() {
+        assertEquals(75000, piano.getSellPrice());
+    }
+
+    @Test
+    public void hasPianoType() {
+        assertEquals(PianoType.GRAND, piano.getPianoType());
+    }
+
+    @Test
+    public void canGetDescription() {
+        assertEquals("Yamaha CF3", piano.getDescription());
+    }
+
+    @Test
+    public void canCalculateMarkup() {
+        assertEquals(25000, piano.calculateMarkup());
+    }
+
+    @Test
+    public void canPlay() {
+        assertEquals("*Tinkle tinkle, tinkly tinkle*", piano.play());
+    }
 }
