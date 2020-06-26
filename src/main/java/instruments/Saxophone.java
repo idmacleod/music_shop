9cb55528d3dcf2d768e2a86cdd2a9c4e5c96c03d
idmacleod <idmacleod@gmail.com>
2020-06-26 18:20:35 +0100

    Implemented Saxophone class

--- src/main/java/instruments/Saxophone.java
+++ src/main/java/instruments/Saxophone.java
@@ -1,4 +1,29 @@
 package instruments;
 
-public class Saxophone {
+import accessories.Reeds;
+import types.Family;
+import types.SaxophoneType;
+
+public class Saxophone extends Instrument {
+    private SaxophoneType saxophoneType;
+    private Reeds reeds;
+
+    public Saxophone(String manufacturer, String model, int buyPrice, int sellPrice, SaxophoneType saxophoneType, Reeds reeds) {
+        super(manufacturer, model, Family.WOODWIND, buyPrice, sellPrice);
+        this.saxophoneType = saxophoneType;
+        this.reeds = reeds;
+    }
+
+    public SaxophoneType getSaxophoneType() {
+        return saxophoneType;
+    }
+
+    public Reeds getReeds() {
+        return reeds;
+    }
+
+    @Override
+    public String play() {
+        return "*Wootee woo woahhh*";
+    }
 }
