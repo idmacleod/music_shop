4d75c7c0dec26c6450fd2bf833f6d2520e579333
idmacleod <idmacleod@gmail.com>
2020-06-26 17:46:10 +0100

    Implemented Piano class

--- src/main/java/instruments/Piano.java
+++ src/main/java/instruments/Piano.java
@@ -1,4 +1,21 @@
 package instruments;
 
-public class Piano {
+import types.Family;
+import types.PianoType;
+
+public class Piano extends Instrument {
+    private PianoType pianoType;
+
+    public Piano(String manufacturer, String model, int buyPrice, int sellPrice, PianoType pianoType) {
+        super(manufacturer, model, Family.KEYBOARD, buyPrice, sellPrice);
+        this.pianoType = pianoType;
+    }
+
+    public PianoType getPianoType() {
+        return pianoType;
+    }
+
+    public String play() {
+        return "*Tinkle tinkle, tinkly tinkle*";
+    }
 }
