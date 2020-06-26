595cfc2b0f65a22e4a57e0e13fdb79ce77d4ef5a
idmacleod <idmacleod@gmail.com>
2020-06-26 18:30:00 +0100

    Added Bow tests

--- src/test/java/accessories/BowTest.java
+++ src/test/java/accessories/BowTest.java
@@ -1,4 +1,40 @@
 package accessories;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BowTest {
+    private Bow bow;
+
+    @Before
+    public void before() {
+        bow = new Bow("Dorfler Master No. 19", 280, 300, "Pernambuco", "Nickel Silver");
+    }
+
+    @Test
+    public void hasDescription() {
+        assertEquals("Dorfler Master No. 19", bow.getDescription());
+    }
+
+    @Test
+    public void hasBuyPrice() {
+        assertEquals(280, bow.getBuyPrice());
+    }
+
+    @Test
+    public void hasSellPrice() {
+        assertEquals(300, bow.getSellPrice());
+    }
+
+    @Test
+    public void hasWood() {
+        assertEquals("Pernambuco", bow.getWood());
+    }
+
+    @Test
+    public void hasMetal() {
+        assertEquals("Nickel Silver", bow.getMetal());
+    }
 }
