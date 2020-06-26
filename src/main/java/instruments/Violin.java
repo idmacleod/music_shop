38517c37bb639368429b033c53666a58bdb99c22
idmacleod <idmacleod@gmail.com>
2020-06-26 18:47:59 +0100

    Implemented Violin class

--- src/main/java/instruments/Violin.java
+++ src/main/java/instruments/Violin.java
@@ -1,4 +1,28 @@
 package instruments;
 
-public class Violin {
+import accessories.Bow;
+import types.Family;
+import types.ViolinType;
+
+public class Violin extends Instrument {
+    private ViolinType violinType;
+    private Bow bow;
+
+    public Violin(String manufacturer, String model, int buyPrice, int sellPrice, ViolinType violinType, Bow bow) {
+        super(manufacturer, model, Family.STRINGS, buyPrice, sellPrice);
+        this.violinType = violinType;
+        this.bow = bow;
+    }
+
+    public ViolinType getViolinType() {
+        return violinType;
+    }
+
+    public Bow getBow() {
+        return bow;
+    }
+
+    public String play() {
+        return "*Screeeee scree chum chum*";
+    }
 }
